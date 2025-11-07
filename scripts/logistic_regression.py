e6d61011579a582b40e5813a2b2541558cf6621a
Anna-Bilicka-Hinteregger <abilickaoff@gmail.com>
2025-11-07 22:17:09 +0100

    added confusion matrix, deleted directory output for report

--- scripts/logistic_regression.py
+++ scripts/logistic_regression.py
@@ -1,6 +1,6 @@
 from sklearn.linear_model import LogisticRegression
 import joblib
-from sklearn.metrics import classification_report, roc_auc_score
+from sklearn.metrics import classification_report, roc_auc_score, confusion_matrix
 
 def logistic_regression(X_train, y_train, X_test, y_test):
     model_lg = LogisticRegression(solver='liblinear', random_state=42, C=0.1, max_iter=100)
@@ -12,7 +12,8 @@ def logistic_regression(X_train, y_train, X_test, y_test):
     joblib.dump(model_lg, 'logistic_regression_model.pkl')
 
     # Return metrics
-    report = classification_report(y_test, y_pred, output_dict=True)
+    report = classification_report(y_test, y_pred, output_dict=False)
     roc_auc = roc_auc_score(y_test, y_pred)
+    confusion_matrix = confusion_matrix(y_test, y_pred)
 
-    return report, roc_auc
+    return report, roc_auc, confusion_matrix
